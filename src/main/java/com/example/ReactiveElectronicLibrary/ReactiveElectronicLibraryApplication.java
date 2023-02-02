93bb28572457e9899033f3f14576a5866a537fbd
danmv24 <danmn119@gmail.com>
2023-02-02 19:52:02 +0300

    project created

---
+++ src/main/java/com/example/ReactiveElectronicLibrary/ReactiveElectronicLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReactiveElectronicLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveElectronicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveElectronicLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReactiveElectronicLibrary/ReactiveElectronicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReactiveElectronicLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveElectronicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
